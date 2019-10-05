677bb5af8a350743eaed3a83a6204156a84a0029
Filipe Jordão <filipe.njrd@gmail.com>
2019-10-05 14:46:30 -0300

    Remove unused namespaces

--- test/clojure_algorithm_club/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-algorithm-club.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-algorithm-club.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
